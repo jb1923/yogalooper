250daabf48f541fd2476a33fe8b4f9dc6d061346
John <jb8540@gmail.com>
2022-09-04 19:38:01 +0100

    Start of yogalooper. I started a new empty project in Androidstudio..I copied countdowntimer/app/src/main and used sed and find to change countdowntimer to yogalooper. Tnen replaced main with the modified main. Reopened yogalooper, cleaned it and rebuilt project and it compiled and ran ok.  So yogatimer -> countdowntimer-> yogalooper.

---
+++ java/com/example/yogalooper/MainActivity.kt
@@ -0,0 +1,268 @@
+package com.example.yogalooper
+
+import android.os.Bundle
+//import android.os.yogalooper
+import android.view.View
+import android.widget.TextView
+import android.app.Activity
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.os.Handler
+import android.view.inputmethod.InputMethodManager
+import android.os.Looper
+import java.lang.Runnable
+import java.util.Locale
+import android.media.ToneGenerator
+import android.media.AudioManager
+//import android.util.AttributeSet
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : Activity() {
+   private var pauseDelay  = 0// pause between loops
+    private var loopTimer  = -pauseDelay // seconds counter for loop timer in t1View?
+    private var relaxationTimer = 0 // seconds counter for t2View relaxation timer
+    private var loop1  = 0// loop1 time = 45"
+    private var loop2  = 0// loop2 time = 60"
+    private var loop0 = loop1 // loop0 =loop time in seconds ie loop1 or loop2 ie 45" or 60"
+    private var runningT1 = false
+    private var runningT2 = false
+    private var setupButton: Button? = null
+    private var loop1Button: Button? = null
+    private var loop2Button: Button? = null
+    private var stopButton: Button? = null
+    private var startT2Button: Button? = null
+    private var clearButton: Button? = null
+    private var labelPause: TextView? = null
+    private var labelLoop1: TextView? = null
+    private var labelLoop2: TextView? = null
+
+    val colWhite = -0x1
+    val buttonOffColor = colWhite
+    val buttonOnColor = -0x100
+    val pauseColor = -0x10000
+    val toneGen1 = ToneGenerator(AudioManager.STREAM_SYSTEM, 100)
+
+    var t1View: TextView? = null
+    var t2View: TextView? = null
+    private var editPause: EditText? = null
+    private var editLoop1: EditText? = null
+    private var editLoop2: EditText? = null
+
+    private  val TAG = "MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        setupButton = findViewById<View>(R.id.setupButton) as Button
+        loop1Button = findViewById<View>(R.id.loop1Button) as Button
+        loop2Button = findViewById<View>(R.id.loop2Button) as Button
+        stopButton = findViewById<View>(R.id.stopButton) as Button
+        startT2Button = findViewById<View>(R.id.startT2Button) as Button
+        clearButton = findViewById<View>(R.id.clearButton) as Button
+        t1View = findViewById(R.id.t1View) as TextView
+        t2View = findViewById(R.id.t2View) as TextView
+        loadData()// load loop1,loop2, pauseDelay from previous run
+        loop1Button!!.text = "loop " + Integer.toString(loop1) //set the text on button
+        loop2Button!!.text = "loop " + Integer.toString(loop2) //set the text on button
+
+        labelPause = findViewById<View>(R.id.labelPause) as TextView
+        labelLoop1 = findViewById<View>(R.id.labelLoop1) as TextView
+        labelLoop2 = findViewById<View>(R.id.labelLoop2) as TextView
+        editPause = findViewById(R.id.editPause) as EditText
+        editLoop1 = findViewById(R.id.editLoop1) as EditText
+        editLoop2 = findViewById(R.id.editLoop2) as EditText
+        // hide setup menu
+        editPause!!.visibility = View.INVISIBLE
+        labelPause!!.visibility = View.INVISIBLE
+        editLoop1!!.visibility = View.INVISIBLE
+        labelLoop1!!.visibility = View.INVISIBLE
+        editLoop2!!.visibility = View.INVISIBLE
+        labelLoop2!!.visibility = View.INVISIBLE
+
+        running_loopTimer()
+
+    }
+
+    private fun running_loopTimer() {
+        val handle1 = Handler(Looper.getMainLooper())
+        handle1.post(object : Runnable {
+//            val t1View?: TextView = findViewById(R.id.t1View?)
+//            var t2View: TextView = findViewById(R.id.t2View)
+            override fun run() {
+                // update loopTimer on screen loop0 = 45 or 60 loop
+                var secs = loopTimer % 60
+                var mins = loopTimer / 60
+                val hrs: Int
+                val time_t1: String
+                if (loop0 > 600) {
+                    t1View?.textSize = 140f
+                    time_t1 = String.format(Locale.getDefault(), "%02d:%02d", mins, Math.abs(secs))
+                } else if (loop0 > 60) {
+                    t1View?.textSize = 160f
+                    time_t1 = String.format(Locale.getDefault(), "%01d:%02d", mins, Math.abs(secs))
+                } else {
+                    t1View?.textSize = 200f
+                    time_t1 = String.format(Locale.getDefault(), "%02d", Math.abs(secs))
+                }
+                t1View?.text = time_t1
+                if (runningT1) {   // set timer1 red for pauseDelay -7" then white for loop 45"
+                    t1View?.setTextColor(if (loopTimer < 0) pauseColor else colWhite)
+                    if (loopTimer == 0) toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+                    // When we reach loop count set timer to count down for pauseDelay seconds
+                    if (loopTimer == loop0) { // reached end of loop ie. 45"
+                        loopTimer = -pauseDelay // timer reset to -7" pauseDelay
+                        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+                    }
+                    loopTimer++ // update loopTimer if not stopped
+                } else { // if runningT1 = false. T1 stopped
+                    t1View?.text = "**"
+                } // end of if (runningT1)
+
+                // update relaxationTimer on screen
+                hrs = relaxationTimer / 3600
+                mins = relaxationTimer % 3600 / 60
+                secs = relaxationTimer % 60
+                val time_t2: String
+                time_t2 = String.format("%02d:%02d:%02d", hrs, mins, Math.abs(secs))
+                t2View?.text = time_t2
+                if (runningT2) {   // set timer2 red for pauseDelay -7" then white
+                    t2View?.setTextColor(if (relaxationTimer < 0) pauseColor else colWhite)
+                    //  if t1 is stopped, then every 600" beep
+                    if (runningT1 == false && relaxationTimer % 600 == 0) toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+                    relaxationTimer++ // update loopTimer as T});2 running
+                } // end of if (runningT2)
+                handle1.postDelayed(this, 1000) // 1" time delay
+            } //= end of public void run
+        }) // end of handle1.post(new Runnable() should be  });//
+    } //  end of loop timer
+
+    fun GetInt(edTxt: EditText, default1: Int): Int {
+        // gets integer from EditText - if no valid int, returns default1
+        // first remove all chars  except numbers
+        val edTxtStr : String  = edTxt.text.toString().replace("[^0-9]+".toRegex(), "")
+        // if edTxtStr ="" return default1 else return user value as integer
+        return if (edTxtStr == "") default1 else edTxtStr.toInt()
+    }
+
+    fun SetInt(edTxt : EditText, value: Int) { //displays  integer value in EditText
+        edTxt.setText(" " + Integer.toString(value) + " ") // add spaces for easy selectin
+    }
+    fun saveData() {
+        // save set up values for next session
+        val sharedPref = getSharedPreferences(MyPREFERENCES, MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putInt("loop1", loop1) // save integer loop1 for next session
+        editor.putInt("loop2", loop2) // save integer loop2 for next session
+        editor.putInt("pauseDelay", pauseDelay) // save pauseDelay for next session
+        editor.commit()
+        editor.apply()
+    }
+
+    fun loadData() {
+        // save set up values between sessions
+        val sharedPref = getSharedPreferences(MyPREFERENCES, MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        loop1 = sharedPref.getInt("loop1", 45) // get loop1 from previous session
+        loop2 = sharedPref.getInt("loop2", 60) // get loop2 from previous session
+        pauseDelay = sharedPref.getInt("pauseDelay", 7) //get pauseDelay from previous session
+        loopTimer = -pauseDelay
+    }
+
+
+    fun onClickSetup(view: View?) {
+        if (editLoop1!!.visibility == View.VISIBLE) {   // save button has been clicked:- update loop1,loop2,pauseDelay, change button label to SETUP
+            pauseDelay = GetInt(editPause!!,7) // CustomEditText.GetInt
+            loopTimer = -pauseDelay // need -ive number for countdown
+            loop1 = GetInt(editLoop1!!,45) // CustomEditText.GetInt
+            loop2 =GetInt( editLoop2!!,60) // CustomEditText.GetInt
+            //     loop0 = loop1;
+            saveData()
+            // Close keyboard
+            (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(editPause!!.windowToken, 0)
+
+            //editPause.setImeOptions(EditorInfo.IME_ACTION_DONE);
+            editPause!!.visibility = View.INVISIBLE
+            labelPause!!.visibility = View.INVISIBLE
+            editLoop1!!.visibility = View.INVISIBLE
+            labelLoop1!!.visibility = View.INVISIBLE
+            editLoop2!!.visibility = View.INVISIBLE
+            labelLoop2!!.visibility = View.INVISIBLE
+            setupButton!!.text = "setup " //set the text on button
+            loop1Button!!.text = "loop " + Integer.toString(loop1) //set the text on button
+            loop2Button!!.text = "loop " + Integer.toString(loop2) //set the text on button
+            t1View!!.visibility = View.VISIBLE
+        } else { // setup button has been clicked:- change button label to SAVE, open edit texts
+            setupButton!!.text = "Save  " //set the text on button
+            SetInt(editLoop1!!,loop1) // SetInt (editText, integer value to set)
+            SetInt(editLoop2!!,loop2) // CustomEditText.SetInt
+            SetInt(editPause!!,pauseDelay) // CustomEditText.SetInt
+            t1View!!.visibility = View.INVISIBLE
+            labelPause!!.visibility = View.VISIBLE
+            editPause!!.visibility = View.VISIBLE
+            editLoop1!!.visibility = View.VISIBLE
+            labelLoop1!!.visibility = View.VISIBLE
+            editLoop2!!.visibility = View.VISIBLE
+            labelLoop2!!.visibility = View.VISIBLE
+            // Open keyboard
+            (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).showSoftInput(editPause, InputMethodManager.SHOW_FORCED)
+            editPause!!.setSelection(editPause!!.text.length)
+        }
+    }
+
+    fun onClickLoop1(view: View?) {
+        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+        runningT1 = true
+        runningT2 = true
+        loop0 = loop1
+        loopTimer = -pauseDelay
+        loop2Button?.setBackgroundColor(buttonOffColor)
+        loop1Button?.setBackgroundColor(buttonOnColor)
+        startT2Button!!.setBackgroundColor(buttonOffColor)
+    }
+
+    fun onClickLoop2(view: View?) {
+        //Button loop2Button = (Button) findViewById(R.id.loop2Button);
+        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+        runningT1 = true
+        runningT2 = true
+        loop0 = loop2
+        loopTimer = -pauseDelay
+        loop1Button?.setBackgroundColor(buttonOffColor)
+        loop2Button?.setBackgroundColor(buttonOnColor)
+        startT2Button!!.setBackgroundColor(buttonOffColor)
+    }
+
+    fun onClickStartT2(view: View?) {
+        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 100)
+        runningT1 = false
+        runningT2 = true
+        loopTimer = 0
+        startT2Button!!.setBackgroundColor(buttonOnColor)
+        loop1Button!!.setBackgroundColor(buttonOffColor)
+        loop2Button!!.setBackgroundColor(buttonOffColor)
+    }
+
+    fun onClickStop(view: View?) {
+        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 100)
+        runningT1 = false
+        runningT2 = false
+        startT2Button!!.setBackgroundColor(buttonOffColor)
+        loop1Button!!.setBackgroundColor(buttonOffColor)
+        loop2Button!!.setBackgroundColor(buttonOffColor)
+    }
+
+    fun onClickClear(view: View?) {
+        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 100)
+        loopTimer = 0
+        relaxationTimer = -pauseDelay
+        t2View?.setTextColor(pauseColor)
+    }
+
+    companion object {
+        const val MyPREFERENCES = "MyPrefs"
+    }
+
+} // end of main activity
